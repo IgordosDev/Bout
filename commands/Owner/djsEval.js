59a646f2fbd5f3a6f786cbe97f1258fa13de6b6a
IgordosYT <69392185+IgordosYT@users.noreply.github.com>
2020-11-10 21:10:15 +0500

    Delete djsEval.js

--- commands/Owner/djsEval.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({
-    name: "eval",
-    code: `
-    $author[djs Evaluate]
-$onlyForIDs[$getVar[Owners];Это только для разрабов]
-$title[Успешно. **$ping** ms.]
-$description[$djsEval[$message]]
-$footer[Вызвано $username]`
-    });

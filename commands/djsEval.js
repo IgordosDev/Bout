44cdc97cd2e8e28cb79aaed3249515ddba3f4c59
IgordosYT <69392185+IgordosYT@users.noreply.github.com>
2020-11-10 17:05:04 +0500

    Create djsEval.js

---
+++ commands/djsEval.js
@@ -0,0 +1,10 @@
+module.exports = ({
+    name: "djsEval",
+    code: `
+    $author[discord.js Evaluate]
+$onlyForIDs[$getVar[Owners];Это только для разрабов]
+$title[Успешно. **$ping** ms.
+$description[$djsEval]
+$footerIcon[$authorAvatar]
+$footer[Вызвано $username]`
+    });

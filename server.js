031548afb6a402a2981fb310b3f11e2be3454719
IgordosYT <69392185+IgordosYT@users.noreply.github.com>
2020-11-10 16:36:57 +0500

    Update server.js

--- server.js
+++ server.js
@@ -11,3 +11,19 @@ bot.command({
 name: "ping", 
 code: `Pong! \`$ping\`` 
 })
+
+bot.onMessage()
+ 
+const fs = require('fs')
+
+const folders = fs.readdirSync("./commands/")
+
+for (const files of folders) {
+const folder = fs.readdirSync(`./commands/${files}/`).filter(file => file.endsWith(".js"))
+
+for (const commands of folder) {
+const command = require(`./commands/${files}/${commands}`) 
+bot.command({
+name: command.name,
+code: command.code
+})

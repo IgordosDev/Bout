73303075187a832c9e41be9f8147e4202b36f63e
IgordosYT <69392185+IgordosYT@users.noreply.github.com>
2020-11-10 17:01:01 +0500

    Create info.js

---
+++ commands/info.js
@@ -0,0 +1,9 @@
+module.exports = ({
+    name: "info",
+    code: `
+    $title[блять бот который посылает нахер других ботов]
+$description[бот придуман [Робертом каким-то там](https://vk.com/) и реализован $username#$discriminator.]
+$addField[тут типа инфа обо мне;ОС — linux
+Задержка — $ping
+Сколько часов я не сплю — $uptime`
+    });

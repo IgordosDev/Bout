e467648b073654efa6d4a3ce425de901ff7548e7
IgordosYT <69392185+IgordosYT@users.noreply.github.com>
2020-11-10 21:13:41 +0500

    Update messageEvent.js

--- commands/Public/messageEvent.js
+++ commands/Public/messageEvent.js
@@ -2,7 +2,7 @@ module.exports = ({
     name: "$alwaysExecute",
     code: `
 $nomention
-    $onlyIf[$isBot[$authorID]==true;]
+    
 $onlyIf[$authorID!=$clientID;]
 <@$authorID>, $randomText[пошёл нахер;иди нахер;завались].`
     });

0430b8ec955d7e5dbdac2526d6a104802a5cc00f
IgordosYT <69392185+IgordosYT@users.noreply.github.com>
2020-11-10 20:16:09 +0500

    Update info.js

--- commands/Public/info.js
+++ commands/Public/info.js
@@ -2,7 +2,7 @@ module.exports = ({
     name: "info",
     code: `
     $title[блять бот который посылает нахер других ботов]
-$description[бот придуман [Робертом каким-то там](https://vk.com/) и реализован $username#$discriminator.]
+$description[бот придуман [Робертом каким-то там\\](https://vk.com/) и реализован $username#$discriminator.]
 $addField[тут типа инфа обо мне;ОС — linux
 Задержка — $ping
 Сколько часов я не сплю — $uptime

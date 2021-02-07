89204ecd98d9198f07dfef5bb4d2b10e209e48a6
Rinary <69392185+IgordosYT@users.noreply.github.com>
2021-02-07 10:59:32 +0500

    Update eval.js

--- commands/Owner/eval.js
+++ commands/Owner/eval.js
@@ -1,9 +1,5 @@
 module.exports = ({
     name: "eval",
-    code: `
-    $author[DBD.js Evaluate]
-$onlyForIDs[$getVar[Owners];Это только для разрабов]
-$title[Успешно. **$ping** ms.]
-$description[$eval[$message]]
-$footer[Вызвано $username]`
+    code: `$eval[$message]
+$onlyForIDs[$getVar[Owner];Это только для разрабов]`
     });

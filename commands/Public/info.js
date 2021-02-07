f5349b75c53bf2ad6683fd17ad25bfe1b870f355
Rinary <69392185+RinaryYT@users.noreply.github.com>
2021-02-07 11:23:52 +0500

    Update info.js

--- commands/Public/info.js
+++ commands/Public/info.js
@@ -1,10 +1,14 @@
 module.exports = ({
     name: "info",
     code: `
-    $title[блять бот который посылает нахер других ботов]
+    $title[бот который посылает нахер других ботов]
 $description[бот придуман [Робертом Растроповым\\](https://vk.com/tailt99) и реализован $userTag[694849711437250581].]
-$addField[тут типа инфа о боте;ОС — linux
+$addField[техническая инфа;ОС — linux
 Задержка — $botPing(websocket — $ping)
 Аптайм — $uptime
 RAM — $ram
-CPU — $cpu]`});
+CPU — $cpu;yes]
+$addField[инфа о боте;всего серверов — $serverCount
+всего ботов — $sub[$membersCount[all;yes];$membersCount[all;no]](людей - $membersCount[all;no])
+[GitHub//](https://github.com/RinaryYT/Bout)
+[пригласи меня на свои сервера//](https://discord.com/oauth2/authorize?client_id=$clientID&scope=bot&permissions=68608);yes]`});

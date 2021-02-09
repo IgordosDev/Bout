dd07da2e4f29670ed662363bbd7c1168eabe2126
Rinary <69392185+RinaryYT@users.noreply.github.com>
2021-02-09 16:05:15 +0500

    Update messageEvent.js

--- commands/Public/messageEvent.js
+++ commands/Public/messageEvent.js
@@ -1,6 +1,6 @@
 module.exports = ({
     name: "$alwaysExecute",
-    code: `$reply[$messageID;<@$authorID>, $randomText[пошёл нахер;иди нахер;завались].;yes]
+    code: `$reply[$messageID;<@$authorID>, $randomText[пошёл нахер;иди нахер;завались;заткнись;пожалуйста, закрой рот;рот закрой;ещё один твой, никому не нужный, ответ].;no]
 $onlyIf[$isBot[$authorID]==true;]
 $onlyIf[$authorID!=$clientID;]`
     });

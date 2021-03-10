c6cf1e653e80795c57364924582384bcaa49e925
Igordos <69392185+IgordosYT@users.noreply.github.com>
2021-03-11 02:05:38 +0500

    исправление пинга на мобайле + 3 фразы

--- commands/Public/messageEvent.js
+++ commands/Public/messageEvent.js
@@ -1,6 +1,6 @@
 module.exports = ({
     name: "$alwaysExecute",
-    code: `$reply[$messageID;<@$authorID>, $randomText[пошёл нахер;иди нахер;завались;заткнись;пожалуйста, закрой рот;рот закрой;ещё один твой, никому не нужный, ответ].;no]
+    code: `$reply[$messageID;<@!$authorID>, $randomText[пошёл нахер;иди нахер;завались;заткнись;пожалуйста, закрой рот;рот закрой;иди нахуй;пошёл нахуй;заткни ебало;иди нахуй, програмная сука].;no]
 $onlyIf[$isBot[$authorID]==true;]
 $onlyIf[$authorID!=$clientID;]`
     });
